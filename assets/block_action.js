0786ee7a1c9733d03ac581103d42da8bc56bd74f
dawn-knight <dawn_knight_xp@hotmail.com>
2018-08-09 17:30:03 +0800

    '20180809-office'

--- assets/block_action.js
+++ assets/block_action.js
@@ -12,26 +12,30 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+		mouseDownTime: 0
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+		this.node.on('mouseup', function(event) {
+			var today = new Date();
+			var timeInterval = today.getTime() - this.mouseDownTime;
+			
+			if (timeInterval > 500) {
+				this.node.rotationX += 180;
+			} else {
+				this.node.rotation += 90;
+			}
+			
+			this.mouseDownTime = 0;
+		}, this);
+		
+		this.node.on('mousedown', function(event) {
+			var today = new Date();
+			this.mouseDownTime = today.getTime();
+		}, this);		
+	},
 
     start () {
 
