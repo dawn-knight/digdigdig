a45dcc589e670484c1c9143d48332557d5ee7178
dawn-knight <dawn_knight_xp@hotmail.com>
2018-08-13 17:34:41 +0800

    '20180813-00-office'

--- assets/block_action.js
+++ assets/block_action.js
@@ -1,55 +1,50 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+var newBlkAction = new (require('block_action2'));
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-		mouseDownTime: 0,
-		rotatedByYAxis: true,
+		// mouseDownTime: 0,
+		// rotatedByYAxis: true,
 		mouseDown: false,
-		moved: false,
+		// moved: false,
 		newBlksGen: false	
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+		if (this.node.name.endsWith('_new'))
+			return;
+		
 		this.node.on('mouseup', function(event) {
 			this.mouseDown = false;
 			
-			if (!this.moved) {
-				var today = new Date();
-				var timeInterval = today.getTime() - this.mouseDownTime;
+			// if (!this.moved) {
+				// var today = new Date();
+				// var timeInterval = today.getTime() - this.mouseDownTime;
 				
-				if (timeInterval > 350) {
-					if (this.rotatedByYAxis) {
-						this.node.rotationY += 180;
-					} else {
-						this.node.rotationX += 180;
-					}
-				} else {
-					this.node.rotationX += 90;
-					this.node.rotationY += 90;
-					this.rotatedByYAxis = !this.rotatedByYAxis;
-				}
-			} else {
-				this.moved = false;
-			}
+				// if (timeInterval > 350) {
+					// if (this.rotatedByYAxis) {
+						// this.node.rotationY += 180;
+					// } else {
+						// this.node.rotationX += 180;
+					// }
+				// } else {
+					// this.node.rotationX += 90;
+					// this.node.rotationY += 90;
+					// this.rotatedByYAxis = !this.rotatedByYAxis;
+				// }
+			// } else {
+				// this.moved = false;
+			// }
 			
-			this.mouseDownTime = 0;
+			// this.mouseDownTime = 0;
 		}, this);
 		
 		this.node.on('mousedown', function(event) {
-			var today = new Date();
-			this.mouseDownTime = today.getTime();
+			// var today = new Date();
+			// this.mouseDownTime = today.getTime();
 			this.mouseDown = true;
 		}, this);
 
@@ -60,32 +55,11 @@ cc.Class({
 			if (this.mouseDown && (Math.abs(offset_x) >= 10 || Math.abs(offset_y) >= 10)) {
 				cc.log('newBlksGen: ' + this.newBlksGen);
 				if (!this.newBlksGen) {
-					if (this.node.name == 'redI') {
-						this.genRedI();
-					} else if (this.node.name == 'yellowZ') {
-						this.genYellowZ();
-					} else if (this.node.name == 'orangeL') {
-						this.genOrangeL();
-					} else if (this.node.name == 'greenT') {
-						this.genGreenT();
-					} else if (this.node.name == 'purpleO') {
-						this.genPurpleO();
-					}
+					this.genNewBlks();
 					this.newBlksGen = true;
 				}
 			}
-		}, this);		
-
-		// this.node.on('mousemove', function(event) {
-			// cc.log('mousemove: ' + this.mouseDown);
-			// if (this.mouseDown) {
-				// cc.log('blocks x: ' + this.node.x + ', y: ' + this.node.y)
-				// cc.log('cursor x: ' + event.getLocationX() + ', y: ' + event.getLocationY())
-				// this.node.x = event.getLocationX();
-				// this.node.y = event.getLocationY();
-				// this.moved = true;
-			// }
-		// }, this);
+		}, this);
 	},
 
     start () {
@@ -94,8 +68,7 @@ cc.Class({
 
     // update (dt) {},
 	
-	genRedI () {
-		cc.log('redI');
+	genNewBlks () {
 		var new_one = new cc.Node(this.node.name + '_new');
 		new_one.x = this.node.x + 10;
 		new_one.y = this.node.y - 10;
@@ -105,62 +78,6 @@ cc.Class({
 		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;
 		new_one.parent = this.node.parent;
 		new_one.setAnchorPoint(0, 0);
-		cc.log(new_one);
-	},
-	
-	genYellowZ () {
-		cc.log('yellowZ');
-		var new_one = new cc.Node(this.node.name + '_new');
-		new_one.x = this.node.x + 10;
-		new_one.y = this.node.y - 10;
-		new_one.rotationX = this.node.rotationX;
-		new_one.rotationY = this.node.rotationY;
-		new_one.addComponent(cc.Sprite);
-		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;		
-		new_one.parent = this.node.parent;
-		new_one.setAnchorPoint(0, 0);		
-		cc.log(new_one);		
-	},
-	
-	genOrangeL () {
-		cc.log('orangeL');
-		var new_one = new cc.Node(this.node.name + '_new');
-		new_one.x = this.node.x + 10;
-		new_one.y = this.node.y - 10;
-		new_one.rotationX = this.node.rotationX;
-		new_one.rotationY = this.node.rotationY;
-		new_one.addComponent(cc.Sprite);
-		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;		
-		new_one.parent = this.node.parent;
-		new_one.setAnchorPoint(0, 0);
-		cc.log(new_one);		
-	},
-	
-	genGreenT () {
-		cc.log('greenT');
-		var new_one = new cc.Node(this.node.name + '_new');
-		new_one.x = this.node.x + 10;
-		new_one.y = this.node.y - 10;
-		new_one.rotationX = this.node.rotationX;
-		new_one.rotationY = this.node.rotationY;
-		new_one.addComponent(cc.Sprite);
-		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;		
-		new_one.parent = this.node.parent;
-		new_one.setAnchorPoint(0, 0);
-		cc.log(new_one);		
-	},
-	
-	genPurpleO () {
-		cc.log('purpleO');
-		var new_one = new cc.Node(this.node.name + '_new');
-		new_one.x = this.node.x + 10;
-		new_one.y = this.node.y - 10;
-		new_one.rotationX = this.node.rotationX;
-		new_one.rotationY = this.node.rotationY;
-		new_one.addComponent(cc.Sprite);
-		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;		
-		new_one.parent = this.node.parent;
-		new_one.setAnchorPoint(0, 0);
-		cc.log(new_one);		
+		cc.log(newBlkAction);
 	},
 });
---
+++ assets/block_action2.js
@@ -0,0 +1,66 @@
+var newBlkAction = cc.Class ({
+    extends: cc.Component,
+	
+	ctor: function newBlkAction () {},
+
+    properties: {
+		mouseDownTime: 0,
+		rotatedByYAxis: true,
+		mouseDown: false,
+		moved: false,	
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+		cc.log('load ' + this.node.name);
+		if (!this.node.name.endsWith('_new'))
+			return;
+		
+		this.node.on('mouseup', function(event) {
+			this.mouseDown = false;
+			
+			if (!this.moved) {
+				var today = new Date();
+				var timeInterval = today.getTime() - this.mouseDownTime;
+				
+				if (timeInterval > 350) {
+					if (this.rotatedByYAxis) {
+						this.node.rotationY += 180;
+					} else {
+						this.node.rotationX += 180;
+					}
+				} else {
+					this.node.rotationX += 90;
+					this.node.rotationY += 90;
+					this.rotatedByYAxis = !this.rotatedByYAxis;
+				}
+			} else {
+				this.moved = false;
+			}
+			
+			this.mouseDownTime = 0;
+		}, this);
+		
+		this.node.on('mousedown', function(event) {
+			var today = new Date();
+			this.mouseDownTime = today.getTime();
+			this.mouseDown = true;
+		}, this);		
+
+		this.node.on('mousemove', function(event) {
+			cc.log('new mousemove: ' + this.mouseDown);
+			if (this.mouseDown) {
+				this.node.x = event.getLocationX();
+				this.node.y = event.getLocationY();
+				this.moved = true;
+			}
+		}, this);
+	},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
