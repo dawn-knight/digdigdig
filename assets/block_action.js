edb8e47249312294d9b15bac1962d0036b69d2ca
Leonard Fei <dawn_knight_xp@hotmail.com>
2018-08-27 10:49:12 +0800

    '2018-08-27-00-wuzhen'

--- assets/block_action.js
+++ assets/block_action.js
@@ -77,21 +77,99 @@ cc.Class({
 		var new_one = new cc.Node(this.node.name + '_new');
 		new_one.width = this.node.width;
 		new_one.height = this.node.height;
-		for(var i = 0; i < 4; i++) {
-			var new_sub_node = new cc.Node(this.node.name + '_new_sub' + i);
-			new_sub_node.setAnchorPoint(0, 0);
-			new_sub_node.parent = new_one;
-			var texture = new_sub_node.addComopnent(cc.Sprite);
-			texture = this.texture;
-		}
+		this.genNewSubNodes(new_one);
 		new_one.x = this.node.x - 10;
 		new_one.y = this.node.y - 10;
 		new_one.rotationX = this.node.rotationX;
 		new_one.rotationY = this.node.rotationY;
-		new_one.addComponent(cc.Sprite);
-		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;
+		// new_one.addComponent(cc.Sprite);
+		// new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;
 		new_one.addComponent(newBlkAction);
 		new_one.parent = this.node.parent;
+		cc.log(new_one);
+	},
+	
+	genNewSubNodes(new_node) {
+		var new_sub_node_arr = new Array(new cc.Node(), new cc.Node(), new cc.Node(), new cc.Node());
+		for(var i = 0; i < 4; i++) {
+			new_sub_node_arr[i].setAnchorPoint(0, 0);
+			new_sub_node_arr[i].parent = new_node;
+			var texture = new_sub_node_arr[i].addComponent(cc.Sprite);
+			texture._spriteFrame = this.texture;
+		}
+		if (new_node.name == 'orangeL_new') {
+			// 50是单个block的宽度，暂时写死
+			new_sub_node_arr[0].x = new_node.width / 2 - 50;
+			new_sub_node_arr[0].y = new_node.height / 2 - 50;
+			
+			new_sub_node_arr[1].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[1].y = new_sub_node_arr[0].y - 50;
+			
+			new_sub_node_arr[2].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[2].y = new_sub_node_arr[1].y - 50;
+			
+			new_sub_node_arr[3].x = new_sub_node_arr[2].x + 50;
+			new_sub_node_arr[3].y = new_sub_node_arr[2].y;
+		}
 		
+		if (new_node.name == 'purpleO_new') {
+			// 50是单个block的宽度，暂时写死
+			new_sub_node_arr[0].x = new_node.width / 2 - 50;
+			new_sub_node_arr[0].y = new_node.height / 2 - 50;
+			
+			new_sub_node_arr[1].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[1].y = new_sub_node_arr[0].y - 50;
+			
+			new_sub_node_arr[2].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[2].y = new_sub_node_arr[1].y - 50;
+			
+			new_sub_node_arr[3].x = new_sub_node_arr[2].x + 50;
+			new_sub_node_arr[3].y = new_sub_node_arr[2].y;
+		}
+
+		if (new_node.name == 'redI_new') {
+			// 50是单个block的宽度，暂时写死
+			new_sub_node_arr[0].x = new_node.width / 2 - 50;
+			new_sub_node_arr[0].y = new_node.height / 2 - 50;
+			
+			new_sub_node_arr[1].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[1].y = new_sub_node_arr[0].y - 50;
+			
+			new_sub_node_arr[2].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[2].y = new_sub_node_arr[1].y - 50;
+			
+			new_sub_node_arr[3].x = new_sub_node_arr[2].x + 50;
+			new_sub_node_arr[3].y = new_sub_node_arr[2].y;
+		}
+
+		if (new_node.name == 'yellowZ_new') {
+			// 50是单个block的宽度，暂时写死
+			new_sub_node_arr[0].x = new_node.width / 2 - 50;
+			new_sub_node_arr[0].y = new_node.height / 2 - 50;
+			
+			new_sub_node_arr[1].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[1].y = new_sub_node_arr[0].y - 50;
+			
+			new_sub_node_arr[2].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[2].y = new_sub_node_arr[1].y - 50;
+			
+			new_sub_node_arr[3].x = new_sub_node_arr[2].x + 50;
+			new_sub_node_arr[3].y = new_sub_node_arr[2].y;
+		}
+
+		if (new_node.name == 'greenT_new') {
+			// 50是单个block的宽度，暂时写死
+			new_sub_node_arr[0].x = new_node.width / 2 - 50;
+			new_sub_node_arr[0].y = new_node.height / 2 - 50;
+			
+			new_sub_node_arr[1].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[1].y = new_sub_node_arr[0].y - 50;
+			
+			new_sub_node_arr[2].x = new_sub_node_arr[0].x;
+			new_sub_node_arr[2].y = new_sub_node_arr[1].y - 50;
+			
+			new_sub_node_arr[3].x = new_sub_node_arr[2].x + 50;
+			new_sub_node_arr[3].y = new_sub_node_arr[2].y;
+		}		
 	},
 });
