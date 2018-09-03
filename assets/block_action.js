016bf9ff2b71facc44ebd6e6d6c12099df9314a6
dawn-knight <dawn_knight_xp@hotmail.com>
2018-09-03 18:09:03 +0800

    '20180903-00-office-lvmama'

--- assets/block_action.js
+++ assets/block_action.js
@@ -44,7 +44,7 @@ cc.Class({
 				// this.moved = false;
 			// }
 			
-			this.mouseDownTime = 0;
+			this.mouseDownTime = 0;			
 		}, this);
 		
 		this.node.on('mousedown', function(event) {
@@ -60,7 +60,8 @@ cc.Class({
 			if (this.mouseDown && (Math.abs(offset_x) >= 10 || Math.abs(offset_y) >= 10)) {
 				cc.log('newBlksGen: ' + this.newBlksGen);
 				if (!this.newBlksGen) {
-					this.genNewBlks();
+					// this.genNewBlks();
+					this.makeACopy();
 					this.newBlksGen = true;
 				}
 			}
@@ -72,6 +73,20 @@ cc.Class({
     },
 
     // update (dt) {},
+	
+	makeACopy () {
+		var new_one = new cc.Node(this.node.name + '_new');
+		new_one.width = this.node.width;
+		new_one.height = this.node.height;
+		new_one.x = this.node.x - 10;
+		new_one.y = this.node.y - 10;
+		new_one.rotationX = this.node.rotationX;
+		new_one.rotationY = this.node.rotationY;
+		new_one.addComponent(cc.Sprite);
+		new_one._components[0]._spriteFrame = this.node._components[0]._spriteFrame;
+		new_one.addComponent(newBlkAction);
+		new_one.parent = this.node.parent;		
+	},
 	
 	genNewBlks () {
 		var new_one = new cc.Node(this.node.name + '_new');
