3b821984de0201073589bb0bcef0805db4b0d4f6
dawn-knight <dawn_knight_xp@hotmail.com>
2018-09-04 19:04:14 +0800

    '20180904-00-office-lvmama'

--- assets/block_action2.js
+++ assets/block_action2.js
@@ -40,6 +40,8 @@ cc.Class ({
 			// }
 			
 			// this.mouseDownTime = 0;
+			
+			this.overlappingCheck();
 		}, this);
 		
 		this.node.on('mousedown', function(event) {				
@@ -62,4 +64,77 @@ cc.Class ({
     },
 
     // update (dt) {},
+	
+	overlappingCheck() {
+		if (this.node.name == 'purpleO_new') {
+			// 50是单个block的宽度，暂时写死
+			cc.log('first block[' + (this.node.x - 50) + ', ' + this.node.y + ']');
+			cc.log('second block[' + this.node.x + ', ' + this.node.y + ']');
+			cc.log('third block[' + (this.node.x - 50) + ', ' + (this.node.y - 50) + ']');
+			cc.log('fourth block[' + this.node.x + ', ' + (this.node.y - 50) + ']');
+			return;
+		}
+		
+		
+		if (this.node.name == 'redI_new') {
+			// 50是单个block的宽度，暂时写死
+			// 只需检查x或者y任意一个轴的旋转值即可，长条的姿态只有水平和竖直两种（旋转），无论有没有围绕某个轴进行过翻转
+			// 如果某个轴的旋转值除以90得到的结果又能被2整除，则说明姿态为水平，然则为竖直
+			var remainder = (this.node.rotationX / 90) % 2;
+			if (remainder == 0) {
+				cc.log('first block[' + (this.node.x - 100) + ', ' + (this.node.y - 25) + ']');
+				cc.log('second block[' + (this.node.x - 50) + ', ' + (this.node.y - 25) + ']');
+				cc.log('third block[' + (this.node.x) + ', ' + (this.node.y - 25) + ']');
+				cc.log('fourth block[' + (this.node.x + 50) + ', ' + (this.node.y - 25) + ']');				
+			} else {
+				cc.log('first block[' + (this.node.x - 25) + ', ' + (this.node.y + 100) + ']');
+				cc.log('second block[' + (this.node.x - 25)+ ', ' + (this.node.y + 50) + ']');
+				cc.log('third block[' + (this.node.x - 25) + ', ' + (this.node.y) + ']');
+				cc.log('fourth block[' + (this.node.x - 25) + ', ' + (this.node.y - 50) + ']');				
+			}
+			return;
+		}
+		
+		if (this.node.name == 'yellowZ_new') {
+			// 50是单个block的宽度，暂时写死
+			if (this.node.rotationX == this.node.rotationY) {
+				// 无翻转
+				// 检查x或者y任意一个轴的旋转值，该值如果除以90得到的结果又能被2整除，则说明姿态为水平，然则为竖直
+				var remainder = (this.node.rotationX / 90) % 2;
+				if (remainder == 0) {
+					cc.log('first block[' + (this.node.x - 75) + ', ' + this.node.y + ']');
+					cc.log('second block[' + (this.node.x - 25) + ', ' + this.node.y + ']');
+					cc.log('third block[' + (this.node.x - 25) + ', ' + (this.node.y - 50) + ']');
+					cc.log('fourth block[' + (this.node.x + 25) + ', ' + (this.node.y - 50) + ']');				
+				} else {
+					cc.log('first block[' + (this.node.x) + ', ' + (this.node.y + 25) + ']');
+					cc.log('second block[' + (this.node.x)+ ', ' + (this.node.y - 25) + ']');
+					cc.log('third block[' + (this.node.x - 50) + ', ' + (this.node.y - 25) + ']');
+					cc.log('fourth block[' + (this.node.x - 50) + ', ' + (this.node.y - 75) + ']');				
+				}				
+			} else {
+				// 有翻转
+				// 如发生过翻转，rotationX与rotationY的值将会不一致，取较小的那个值来确定旋转的角度，哪个值较大也意味着图形沿着对应的轴发生了翻转，事实上，该图形水平翻转和垂直翻转得到的姿态是一致的
+				var rotationDegree = (this.node.rotationX > this.node.rotationY) ? this.node.rotationY : this.node.rotationX;
+				var remainder = (rotationDegree / 90) % 2;
+				if (remainder == 0) {
+					cc.log('first block[' + (this.node.x + 25) + ', ' + this.node.y + ']');
+					cc.log('second block[' + (this.node.x - 25) + ', ' + this.node.y + ']');
+					cc.log('third block[' + (this.node.x - 25) + ', ' + (this.node.y - 50) + ']');
+					cc.log('fourth block[' + (this.node.x - 75) + ', ' + (this.node.y - 50) + ']');						
+				} else {
+					cc.log('first block[' + (this.node.x - 50) + ', ' + (this.node.y + 25) + ']');
+					cc.log('second block[' + (this.node.x - 50)+ ', ' + (this.node.y - 25) + ']');
+					cc.log('third block[' + (this.node.x) + ', ' + (this.node.y - 25) + ']');
+					cc.log('fourth block[' + (this.node.x) + ', ' + (this.node.y - 75) + ']');					
+				}
+			}
+			return;
+		}		
+		
+		if (this.node.name == 'greenT_new') {
+			// 50是单个block的宽度，暂时写死
+			return;
+		}
+	}
 });
